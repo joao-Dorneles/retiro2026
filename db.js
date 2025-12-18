ed1860ed248df0c9e4d0d3473df25cb31f3ee812
joao-Dorneles <joaopedrodvv@gmail.com>
2025-12-18 11:19:41 -0300

    teste bd postgrees

--- db.js
+++ db.js
@@ -4,7 +4,9 @@ const { Pool } = require('pg');
 // Configuração que funciona tanto no seu PC quanto no Render
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 // Teste de conexão (opcional, mas bom pra debug)
